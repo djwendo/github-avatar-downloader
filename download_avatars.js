c16230b0349d9ba1ee9a1ac6bba42db777247ccb
Wendy Braun <wendocalrissian@gmail.com>
2018-03-20 01:53:09 +0000

    Modify the callback function to iterate over the results and log the value for each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -6,21 +6,21 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    json: true,
     headers: {
       'User-Agent': 'request',
       'Authorization': token.GITHUB_TOKEN,
     }
   };
 
-  request(options, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, data) {
+    cb(err, data);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  for (var i = 0; i < result.length; i++) {
+    console.log(result[i].avatar_url);
+  }
 });
-
-
-
